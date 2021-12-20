c6c6be960b9aa21d414b99702cdd7b17a90871ec
Milan <milanbojicc@gmail.com>
2021-12-20 18:30:03 +0100

    working on splash screen, replace resources

--- app/src/main/java/com/csi/activities/SplashActivity.kt
+++ app/src/main/java/com/csi/activities/SplashActivity.kt
@@ -36,7 +36,6 @@ class SplashActivity : AppCompatActivity() {
                     return@subscribe
 
                 }
-                Log.i("XXX", count.toString())
                 val drawable = logoArray.getDrawable(count)
                 splashLogo.setImageDrawable(drawable)
                 count += 1
